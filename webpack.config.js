6c9b957134dca0c36e3dad67d77a5fde2aa444e9
Zhen Yong <zhenyong0802@gmail.com>
2021-03-28 00:11:13 +0800

    setup-test-server

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const nodeExternals = require('webpack-node-externals');
+const WebpackShellPlugin = require('webpack-shell-plugin-next');
+const path = require('path');
+const {
+  NODE_ENV = 'production',
+} = process.env;
+module.exports = {
+  entry: './src/index.ts',
+  mode: NODE_ENV,
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'ts-loader',
+        ]
+      }
+    ]
+  },
+  externals: [ nodeExternals() ],
+  watch: NODE_ENV === 'development',
+  plugins: [
+    new WebpackShellPlugin({
+      onBuildEnd: {
+        scripts: ['npm run serve']
+      }
+    })
+  ]
+}
